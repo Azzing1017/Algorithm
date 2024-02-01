50df12e292e6e6ceae08ae93276beaf634d4263b
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-01 13:19:13 +0900

    [Silver I] Title: 스타트와 링크, Time: 1288 ms, Memory: 117556 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,25 +5,26 @@ gap_sum_min = 10000
 
 def gogo(index, start, N):
     global list_input, list_start, list_link, gap_sum_min
-    if index == N/2:
+    if N-start < N/2-index:
+        return 
+    elif index == N/2:
         sum_start=0
         sum_link=0
         for a in range(N):
             for b in range(N):
                 if (list_start[a]==list_start[b]==0) and (a!=b):
                     sum_start += list_input[a][b]
-        for c in range(N):
-            for d in range(N):
-                if (list_start[c]==list_start[d]==1) and (c!=d):
-                    sum_link += list_input[c][d]
+                elif (list_start[a]==list_start[b]==1) and (a!=b):
+                    sum_link += list_input[a][b]
         gap_sum = abs(sum_start-sum_link)
         if gap_sum_min > gap_sum:
             gap_sum_min = gap_sum
-                
-    for i in range(start, N):
-        list_start[i] = 1
-        gogo(index+1, i+1, N)
-        list_start[i] = 0
+        return
+    else:
+        for i in range(start, N):
+            list_start[i] = 1
+            gogo(index+1, i+1, N)
+            list_start[i] = 0
 
 gogo(0, 0, N)
 print(gap_sum_min)
