e8fef62cbb8fd2229febdcb4b83b6fa5f7af57fa
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-01 17:49:49 +0900

    [Silver III] Title: 이전 순열, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,35 @@
+N = int(input())
+list_input = list(map(int, input().split()))
+
+num = 0
+for i in range(-1, -N-1, -1):
+    if i == -N:
+        num=0
+    else:
+        if list_input[i] > list_input[i-1]:
+            continue
+        elif list_input[i] < list_input[i-1]:
+            num = i-1
+            break
+
+if num == 0:
+    print(-1)
+else:
+    list_a = list_input[:num]
+    list_b = list_input[num:num+1]
+    list_c = list_input[num+1:]
+    list_c_temp = list_input[num+1:]
+    list_answer = []
+
+    n=-1
+    while True:
+        if list_b[0] > list_c[n]:      
+            list_c[n] = list_b[0]
+            list_b[0] = list_c_temp[n]
+            list_c.sort(reverse=True)
+            list_answer = list_a + list_b + list_c
+            break
+        else:
+            n -= 1
+    
+    print(' '.join(map(str, list_answer)))
