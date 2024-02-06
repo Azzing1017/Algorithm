9cd434126b1ae94fd04133689cf4c0e3f7d9c5ad
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-07 00:01:22 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 132 ms, Memory: 110408 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,13 @@
+N = int(input())
+li = list(map(int, [0]+input().split()))
+li_ans = [0, li[1]]
+
+for i in range(2, N+1):
+    li_temp = []
+    li_temp.append(li[i])
+    for j in range(1, i//2+1):
+        li_temp.append(li_ans[i-j]+li_ans[j])
+    li_temp.sort(reverse=True)
+    li_ans.append(li_temp.pop())
+
+print(li_ans.pop())
