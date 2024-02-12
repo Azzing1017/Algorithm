f0a30d59ddf74307583ec054b1e3c68c82ca13e3
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-13 03:15:36 +0900

    [Silver II] Title: 좌표 압축, Time: 844 ms, Memory: 290068 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,19 +1,7 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
-li = list([i, int(x), 0] for i, x in enumerate(sys.stdin.readline().split()))
-li.sort(key=lambda x:x[1])
+arr = list(map(int, sys.stdin.readline().split()))
+dic = {x : i for i, x in enumerate(sorted(set(arr)))}
 
-cur = li[0][1]
-num = 0
-for i in range(1, N):
-    if li[i][1] == cur:
-        li[i][2] = num
-    elif li[i][1] > cur:
-        cur = li[i][1]
-        num += 1
-        li[i][2] = num
-li.sort(key=lambda x:x[0])
-
-for var in li:
-    sys.stdout.write(str(var[2]) + ' ')
+sys.stdout.write(' '.join(map(str, [dic[x] for x in arr])))
