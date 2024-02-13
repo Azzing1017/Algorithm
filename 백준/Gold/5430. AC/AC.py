52e6739522637ed032b208a81b03dc205afbb3b0
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-14 03:14:40 +0900

    [Gold V] Title: AC, Time: 624 ms, Memory: 213296 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,29 @@
+import sys
+import re
+from collections import deque
+
+T = int(sys.stdin.readline().strip())
+for _ in range(T):
+    cmd = deque(sys.stdin.readline().strip())
+    N = int(sys.stdin.readline().strip())
+    li = deque(map(int, re.findall(r'\d+', sys.stdin.readline().strip())))
+    cnt = 0
+    check = True
+    while cmd:
+        x = cmd.popleft()
+        if x == 'R':
+            cnt += 1
+        elif x == 'D':
+            if li:
+                if cnt % 2 == 0:
+                    li.popleft()
+                elif cnt % 2 == 1:
+                    li.pop()
+            else:
+                sys.stdout.write('error' + '\n')
+                check = False
+                break
+    if check:
+        if cnt % 2 == 1:
+            li.reverse()
+        sys.stdout.write('['+','.join(map(str, li))+']'+'\n')
