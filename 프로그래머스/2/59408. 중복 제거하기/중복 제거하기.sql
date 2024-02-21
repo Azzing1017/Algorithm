bd8be1d7ac14f69367c96290bfd728b9fa982480
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-22 08:48:33 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,7 +1,5 @@
--- 코드를 입력하세요
-SELECT COUNT(A.NAME)
-FROM (
-    SELECT DISTINCT NAME
-    FROM ANIMAL_INS
-    WHERE NAME IS NOT NULL
-) AS A
+-- count(*): 전체행 숫자 계산일때는 null을 포함합니다.
+-- 하지만 특정 컬럼의 레코드 갯수를 셀 때는 null을 포함하지 않고 셉니다.
+-- distinct절은 중복을 제거한다.
+SELECT COUNT(DISTINCT NAME) count
+FROM ANIMAL_INS;
