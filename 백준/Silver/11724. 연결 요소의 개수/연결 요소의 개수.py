9f2349e57d1c04fdc9d4a1f71d1bc372590b847e
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-25 09:33:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 464 ms, Memory: 229252 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,29 +1,49 @@
+import sys
 from collections import deque
 
-N, M = map(int, input().split())
-li = [[]for _ in range(N+1)]
-for _ in range(M):
-    x, y = map(int, input().split())
-    li[x].append(y)
-    li[y].append(x)
-tf = [False] * (N+1)
-que = deque([])
-cnt = 0
+readlines = sys.stdin.readlines()
+N, M = map(int, readlines[0].strip().split())
+dic = {n: [] for n in range(N+1)}
+for readline in readlines[1:]:
+    x, y = map(int, readline.strip().split())
+    dic[x].append(y)
+    dic[y].append(x)
+
+
+def dfs(num_cur):
+    visited[num_cur] = 1
+    for num_next in dic[num_cur]:
+        if visited[num_next] == 0:
+            dfs(num_next)
+
+
+visited = [0] * (N+1)
+cnt_dfs = 0
 for i in range(1, N+1):
-    if tf[i]:
-        continue
-    else:
-        que.append(i)
-        tf[i] = True
-        cnt += 1
-        while que:
-            temp = que.popleft()
-            for j in li[temp]:
-                if tf[j]:
-                    continue
-                else:
-                    que.append(j)
-                    tf[j] = True
+    if visited[i] == 0:
+        cnt_dfs += 1
+        dfs(i)
 
-print(cnt)
+sys.stdout.write(str(cnt_dfs))
 
+# 
+# def bfs(num_start):
+#     que = deque([])
+#     que.append(num_start)
+#     visited[num_start] = 1
+#     while que:
+#         num_cur = que.popleft()
+#         for num_next in dic[num_cur]:
+#             if visited[num_next] == 0:
+#                 visited[num_next] = 1
+#                 que.append(num_next)
+# 
+# 
+# visited = [0] * (N + 1)
+# cnt_bfs = 0
+# for i in range(1, N+1):
+#     if visited[i] == 0:
+#         cnt_bfs += 1
+#         bfs(i)
+# 
+# sys.stdout.write(str(cnt_bfs))
