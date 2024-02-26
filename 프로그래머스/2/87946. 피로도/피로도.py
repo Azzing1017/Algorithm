5dcd39c7662008946a5fa870727cbc00cb6a05e8
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-26 13:39:00 +0900

    [level 2] Title: 피로도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,11 +1,11 @@
-from collections import deque
-
 def solution(k, dungeons):
     global answer
     answer = -1
+    
     for i in range(len(dungeons)):
         dungeons[i].append(dungeons[i][0] - dungeons[i][1])
     dungeons.sort(key=lambda x: (-x[2], -x[0]))
+    
     
     def go(idx, cur, cnt):
         global answer
@@ -15,6 +15,7 @@ def solution(k, dungeons):
             m, n, k = dungeons[i]
             if cur >= m:
                 go(i+1, cur-n, cnt+1)
+    
     
     go(0, k, 0)
 
