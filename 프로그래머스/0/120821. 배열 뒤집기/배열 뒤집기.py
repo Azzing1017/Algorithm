720cbf12a9d1f0fede2e3ec368bc1a0fab28b7f0
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-26 10:31:47 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,4 @@
 def solution(num_list):
     answer = []
-    for i in range(0, len(num_list)//2):
-        num_list[i], num_list[-1-i] = num_list[-1-i], num_list[i]
-    answer = num_list
+    answer = num_list[::-1]
     return answer
