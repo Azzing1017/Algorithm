890f6e11be8012a94ce3521e96586e9845c3e8e4
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-28 01:13:33 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,7 +1,4 @@
 def solution(x1, x2, x3, x4):
     answer = True
-    if (x1 or x2) and (x3 or x4):
-        answer = True
-    else:
-        answer = False
+    answer = (x1 | x2) & (x3 | x4)
     return answer
