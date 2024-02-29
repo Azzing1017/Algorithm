f22f2c745439d51e21ff18180a7e9be42152c286
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-03-01 02:35:34 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,11 @@
+from collections import defaultdict
+
 def solution(clothes):
     answer = 1
-    dic = {}
-    for var in clothes:
-        x = var[1]
-        if x in dic:
-            dic[x] += 1
-        else:
-            dic[x] = 1
-    for val in dic.values():
-        answer *= (val + 1)
+    dic = defaultdict(int)
+    for v, k in clothes:
+        dic[k] += 1
+    for v in dic.values():
+        answer *= (v + 1)
     answer -= 1
     return answer
