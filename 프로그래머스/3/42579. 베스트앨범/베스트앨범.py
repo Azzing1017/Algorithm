b4e7756cae1d5a8b9e3d9ca4010b4bd6cb7503cb
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-03-01 02:48:26 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,19 @@
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    list_length = len(genres)
-    dic = {}
-    for i in range(list_length):
-        if genres[i] in dic:
-            dic[genres[i]][0] += plays[i]
-            dic[genres[i]][1].append([plays[i], i])
-        else:
-            dic[genres[i]] = [plays[i], [[plays[i], i]]]
-    dic = dict(sorted(dic.items(), key=lambda x:-x[1][0]))
-    for v in dic.values():
-        v[1].sort(key=lambda x:-x[0])
-        cnt = 0
-        for val in v[1]:
-            answer.append(val[1])
-            cnt += 1
-            if cnt == 2:
-                break
+    
+    dic1 = defaultdict(int)
+    dic2 = defaultdict(list)
+    for i, val in enumerate(zip(genres, plays)):
+        k, v = val
+        dic1[k] += v
+        dic2[k].append([v, i])
+    dic1 = dict(sorted(dic1.items(), key=lambda x: -x[1]))
+    
+    for k in dic1.keys():
+        dic2[k].sort(key=lambda x: -x[0])
+        for a, idx in dic2[k][:2]:
+            answer.append(idx)
+    
     return answer
