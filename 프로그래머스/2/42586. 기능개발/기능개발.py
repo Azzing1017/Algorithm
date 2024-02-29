c026f6529856c1640620eec90c8be7a400bc2593
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-03-01 03:11:14 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,14 @@
-from collections import deque
-
 def solution(progresses, speeds):
     answer = []
-    ll = len(progresses)
-    tmp = deque([])
-    for i in range(ll):
-        remain_day = (100 - progresses[i])//speeds[i]
-        if (100 - progresses[i])%speeds[i] != 0:
-            remain_day += 1
-        tmp.append(remain_day)
-    if ll == 1:
-        answer = [1]
-    else:
-        t = tmp.popleft()
-        cnt = 1
-        for _ in range(ll-1):
-            x = tmp.popleft()
-            if t >= x:
-                cnt += 1
-            else:
-                answer.append(cnt)
-                t = x
-                cnt = 1
-        answer.append(cnt)
+    tmp = []
+    answer = [1]
+    tmp.append(-((-(100-progresses[0]))//speeds[0]))
+    for i in range(1, len(progresses)):
+        day = -((progresses[i]-100)//speeds[i])
+        if tmp[-1] >= day:
+            answer[-1] += 1
+        else:
+            tmp.append(day)
+            answer.append(1)
         
     return answer
