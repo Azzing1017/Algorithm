e3ed611b098d7c4c9513602a71959be80146f71d
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-03-01 05:12:12 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,12 +1,8 @@
 def solution(citations):
     answer = 0
-    list_length = len(citations)
+    n = len(citations)
     citations.sort(reverse = True)
-    h = 0
-    for i in range(list_length):
-        h = i + 1
-        if citations[i] >= h:
-            continue
-        else:
-            return h - 1
-    return list_length
+    for i, h in enumerate(citations):
+        if i+1 > h:
+            return i
+    return n
