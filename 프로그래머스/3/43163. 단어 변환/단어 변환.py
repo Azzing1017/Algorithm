81b69b2f04e6fce67f19f478b2529db696946695
이건중 <141884493+Azzing1017@users.noreply.github.com>
2024-02-29 11:36:32 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,23 +1,30 @@
 from collections import deque
 
+
+def get_adjacent(current, words):
+    for word in words:
+        if len(current) != len(word):
+            continue
+
+        count = 0
+        for c, w in zip(current, word):
+            if c != w:
+                count += 1
+
+        if count == 1:
+            yield word
+
+
 def solution(begin, target, words):
-    if target not in words:
-        return 0
-    l = len(target)
-    que = deque([])
-    que.append([begin, 0])
-    while que:
-        word_pre, cnt = que.pop()
-        for word in words:
-            c = 0
-            for i in range(l):
-                if word_pre[i] != word[i]:
-                    c += 1
-                if c > 1:
-                    break
-            if c == 1:
-                if word == target:
-                    return cnt+1
-                que.append([word, cnt+1])
-    if not que:
-        return 0
+    dist = {begin: 0}
+    queue = deque([begin])
+
+    while queue:
+        current = queue.popleft()
+
+        for next_word in get_adjacent(current, words):
+            if next_word not in dist:
+                dist[next_word] = dist[current] + 1
+                queue.append(next_word)
+
+    return dist.get(target, 0)
